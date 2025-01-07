8fbd90ad83a426b2c56124ec18ab247575ee9950
wenc <wncbtrnfl@gmail.com>
2025-01-07 23:35:30 +0800

    added: system page

--- blueprints/main.py
+++ blueprints/main.py
@@ -1,6 +1,6 @@
 import json
 import os
-from flask import Blueprint, render_template, jsonify, url_for, current_app
+from flask import Blueprint, render_template, jsonify, current_app
 
 main = Blueprint('main', __name__, template_folder='templates', static_folder='static')
 
@@ -13,9 +13,22 @@ def index():
 
     return render_template('index.html', objectives=data)
 
+@main.route('/system')
+def system():
+    json_path = os.path.join(current_app.root_path, 'static', 'data', 'accounts.json')
+    
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+
+    return render_template('system.html', accounts=data)
+
 @main.route('/documents')
 def documents():
     return render_template('documents.html')
+
+@main.route('/demo')
+def demo():
+    return render_template('demo.html')
 
 @main.route('/about-us')
 def about():
